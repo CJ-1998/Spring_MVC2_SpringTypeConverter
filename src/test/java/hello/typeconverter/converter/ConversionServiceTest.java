135c17a5d1bbf6325a32a7885d87f5b717592dda
CJ <choijun0627@konkuk.ac.kr>
2023-08-18 18:03:05 +0900

    <feat> 스프링 MVC 2편 프로젝트 sectoin 10
    스프링에 Converter 적용
    
    스프링에서 제공하는 ConversionService에 컨버터를 추가하기 위해
    WebConfig에 addFormatters 함수 사용해서 컨버터 추가
    추가한 컨버터들이 잘 동작하는지 확인하기 위해 HelloController에 컨트롤러
    추가

--- src/test/java/hello/typeconverter/converter/ConversionServiceTest.java
+++ src/test/java/hello/typeconverter/converter/ConversionServiceTest.java
@@ -10,16 +10,16 @@ public class ConversionServiceTest {
     void conversionService() {
         //등록
         DefaultConversionService conversionService = new DefaultConversionService();
-
+        
         conversionService.addConverter(new StringToIntegerConverter());
         conversionService.addConverter(new IntegerToStringConverter());
         conversionService.addConverter(new StringToIpPortConverter());
         conversionService.addConverter(new IpPortToStringConverter());
-
+        
         //사용
         assertThat(conversionService.convert("10", Integer.class)).isEqualTo(10);
         assertThat(conversionService.convert(10, String.class)).isEqualTo("10");
-        
+
         IpPort ipPort = conversionService.convert("127.0.0.1:8080", IpPort.class);
         assertThat(ipPort).isEqualTo(new IpPort("127.0.0.1", 8080));
 
